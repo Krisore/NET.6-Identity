f44f656d0e198f36a376116d810a5e70c9128b26
Enore, Kristian Paul Dizon <61934507+Krisore@users.noreply.github.com>
2023-01-13 18:41:08 +0800

    Add project files.

---
+++ Server/Data/Migrations/20230113102954_CreateIdentitySchemaMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Server.Data.Migrations
+{
+    public partial class CreateIdentitySchemaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
